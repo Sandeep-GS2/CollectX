9e425e672f05b3897dde3bb4aaa65205ca23211a
G S <2480133@cognizant.com>
2026-03-12 18:04:54 +0530

    portfolio1

---
+++ Backend/portfolio-ingestion-service/portfolioingestion/src/main/java/com/cognizant/portfolioingestion/PortfolioingestionApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.portfolioingestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioingestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioingestionApplication.class, args);
+	}
+
+}
